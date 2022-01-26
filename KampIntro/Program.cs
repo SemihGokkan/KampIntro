cf0ebc2b8aaa78e4aa4600f2b245b1e294b23312
SemihG <SemihG@DESKTOP-7HDMA88>
2022-01-27 00:33:12 +0300

    Add project files.

---
+++ KampIntro/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "Kategori";
+            int ogranciSayisi = 32000;
+
+            bool sistemeGrisYapmisMi = true;
+            double  dolarDun = 7.35;
+            double  dolarBugun = 7.45;
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi Butonu");
+            }
+
+            if (sistemeGrisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı Ayarları Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş Yap Butonu");
+            }
+            Console.WriteLine(kategoriEtiketi);
+            
+        }
+    }
+}
